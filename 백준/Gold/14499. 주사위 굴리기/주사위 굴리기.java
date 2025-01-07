f18f87b7a1c5d5ca81dc640f97bdcad672682c5c
nahyon <62339370+nahyon@users.noreply.github.com>
2025-01-07 21:32:17 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 72 ms, Memory: 11860 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -6,8 +6,8 @@ import java.util.StringTokenizer;
 public class Main {
 	static StringBuilder sb = null;
 	static StringTokenizer st = null;
-	static int [][] map ;
-	static int [][] diceMap ; 
+	static byte [][] map ;
+	static byte [][] diceMap ; 
 	static int N, M, x, y;
 
 	public static void main(String[] args) throws IOException {
@@ -16,56 +16,54 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][M]; 
+		map = new byte[N][M]; 
 		
 		x = Integer.parseInt(st.nextToken());
 		y = Integer.parseInt(st.nextToken());
 		
-		int K = Integer.parseInt(st.nextToken()); //명령 K번
+		int K = Integer.parseInt(st.nextToken()); // 명령 K번
 		
 		// 맵 입력
-		for (int i = 0 ; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M ; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Byte.parseByte(st.nextToken());
 			}
 		}
 		
 		// 주사위 맵
-		diceMap = new int[4][3];
-//		diceMap[0][1] = 2; diceMap[1][0] = 4; diceMap[1][1] = 1; diceMap[1][2] = 3;
-//		diceMap[2][1] = 5; diceMap[3][1] = 6;
+		diceMap = new byte[4][3];
 		
 		sb = new StringBuilder();
 		// 명령 K번
 		st = new StringTokenizer(br.readLine());
 		// 1: -> (0, +1) / 2: <- (0, -1) / 3: ^ (-1, 0) / 4: 아래 (+1, 0)
-		for (int i = 0 ; i < K ; i++) {
-			int direction = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < K; i++) {
+			byte direction = (byte) Integer.parseInt(st.nextToken());
 			
 			int nextX = x; int nextY = y;
 			switch(direction) {
 				case 1:
-					nextY = y+1;
+					nextY = y + 1;
 					break;
 				case 2:
-					nextY = y-1;
+					nextY = y - 1;
 					break;
 				case 3:
-					nextX = x-1;
+					nextX = x - 1;
 					break;
 				case 4:
-					nextX = x+1;
+					nextX = x + 1;
 					break;
 			}
 			if (!outOfIndex(nextX, nextY)) continue; // 다음 명령으로
-			//x, y 업데이트
+			// x, y 업데이트
 			x = nextX; y = nextY;
 			
 			rollingDice(direction);
-			//윗면 숫자 sb에 append
+			// 윗면 숫자 sb에 append
 			sb.append(diceMap[1][1]).append("\n");
-			//아랫면 판단 diceMap[3][1]
+			// 아랫면 판단 diceMap[3][1]
 			if (map[nextX][nextY] == 0) map[nextX][nextY] = diceMap[3][1];
 			else {
 				diceMap[3][1] = map[nextX][nextY];
@@ -79,8 +77,8 @@ public class Main {
 		if (n < 0 || m < 0 || n >= N || m >= M ) return false;
 		return true;
 	}
-	static void rollingDice(int direction) {
-		int temp = diceMap[1][1];
+	static void rollingDice(byte direction) {
+		byte temp = diceMap[1][1];
 		switch(direction) {
 			case 1: 
 				diceMap[1][1] = diceMap[1][0];
