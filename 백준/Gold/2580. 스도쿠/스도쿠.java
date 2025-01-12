ba21c31b8b22c8b23ccbc53752be4b35be0c71be
nahyon <62339370+nahyon@users.noreply.github.com>
2025-01-12 21:54:25 +0900

    [Gold IV] Title: 스도쿠, Time: 328 ms, Memory: 15284 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -33,88 +33,31 @@ public class Main {
 			}
 		}
 		cnt = blankList.size();	
-/*		for (int i = 0 ; i < cnt; i++) {
-			Node blank = blankList.get(i);
-			for (int num = 1; num <= 9; num++) {
-				board[blank.r][blank.c] = num; // 보드판에 수 채워넣음
-				if (!isOK(blank.r, blank.c)) continue;
-				break;
-//				if (isOK(blank.r, blank.c)) {
-//					board[blank.r][blank.c] = i; 
-//					dfs(depth+1);
-//				}
-			}
-		}*/
 		
 		dfs(0);
 		
 //		printBoard();
 	}
-	
-	
-	static void dfs(int depth) {
-		if (depth == cnt) {
-//			printBoard();
-//			return;
-	        printBoard();
-	        System.exit(0); // 정답을 찾은 경우 프로그램 종료
-		}
-		
-		Node blank = blankList.get(depth);
-	    int r = blank.r;
-	    int c = blank.c;
-		for (int i = 1; i <= 9; i++) {
-//			board[blank.r][blank.c] = i; // 보드판에 수 채워넣음
-//			if (!isOK(blank.r, blank.c)) continue;
-//			dfs(depth+1);
-	        if (isSafe(r, c, i)) { // 숫자 i가 유효한 경우에만 진행
-	            board[r][c] = i; // 숫자 배치
-	            dfs(depth + 1);  // 다음 빈칸 탐색
-	            board[r][c] = 0; // 백트래킹을 위해 복구
-	        }
-		}
-		
-	}
+    
+    static boolean dfs(int depth) {
+        if (depth == cnt) {
+            printBoard();
+            return true; // 정답을 찾으면 true 반환
+        }
 
-/*	0.0 0.1 0.2 | 0.3 0.4 0.5 | 0.6 0.7 0.8
-	1
-	2
-	
-	3.0 3.1 3.2 | 3.3 3.4 3.5 | 3.6 3.7 3.8
-	4
-	5
-	
-	6.0 6.1 6.2 | 6.3
-	7
-	8
-	*/
-	
-	static boolean isOK(int r, int c) {
-		boolean[] numbers = new boolean[10]; //[1] ~ [9]  사용
-		// 세로 검사 (c열)
-		for (int i = 0 ; i < 9 ; i++ ) {
-			if (board[i][c] == 0) continue; //아직 안 채워진 부분
-			if (numbers[board[i][c]]) return false;
-			numbers[board[i][c]] = true; //방문처리
-		}		
-		// 가로 검사 (r행)
-		numbers = new boolean[10];  // 다시 초기화 
-		for (int j = 0 ; j < 9 ; j++ ) {
-			if (board[r][j] == 0) continue;
-			if (numbers[board[r][j]]) return false;
-			numbers[board[r][j]] = true; //방문처리
-		}		
-		// 3*3 검사
-		numbers = new boolean[10];  // 다시 초기화 
-		for (int i = (r/3)*3 ; i < (r/3)*3+3; i++) {
-			for (int j = (c/3)*3 ; j < (c/3)*3+3 ; j++) {
-				if (board[i][j] == 0) continue;
-				if (numbers[board[i][j]]) return false;
-				numbers[board[i][j]] = true; //방문처리
-			}
-		}
-		return true;
-	}
+        Node blank = blankList.get(depth);
+        int r = blank.r;
+        int c = blank.c;
+        for (int i = 1; i <= 9; i++) {
+            if (isSafe(r, c, i)) { // 숫자 i가 유효한 경우에만 진행
+                board[r][c] = i; // 숫자 배치
+                if (dfs(depth + 1)) return true; // 다음 빈칸 탐색에서 정답을 찾으면 true 반환
+                board[r][c] = 0; // 백트래킹을 위해 복구
+            }
+        }
+        return false; // 모든 숫자를 시도했지만 정답을 찾지 못한 경우 false 반환
+    }
+
 	// 숫자가 r, c 위치에 배치될 수 있는지 확인하는 함수
 	static boolean isSafe(int r, int c, int num) {
 	    // 가로 확인
@@ -135,7 +78,6 @@ public class Main {
 	    }
 	    return true;
 	}
-	
 	
 	static void printBoard() {
 		for (int i = 0 ; i < 9; i++) {
