0dad0757565e18ec126c4a7ebe688373b4814d47
nahyon <62339370+nahyon@users.noreply.github.com>
2025-01-15 02:32:49 +0900

    [Gold III] Title: 파티, Time: 140 ms, Memory: 17028 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 // 백준 1238 _ 파티
-// 다익스트라
+// 다익스트라 (그래프 표현 - 인접리스트VER)
 public class Main {
 	static class Node implements Comparable<Node> {
 		int idx, w;
@@ -19,8 +20,6 @@ public class Main {
 			return this.w - o.w;
 		}
 	}
-//	static ArrayList<Node>[] graph;
-	static int[][] graph;
 	static int[] dist1, dist2;
 	static int INF = 999_999_999;
 	static int N; //노드개수
@@ -32,16 +31,18 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken()); // M개의 간선
 		int destIdx = Integer.parseInt(st.nextToken()); // 목적지
 		
-		//그래프 초기화
-//		graph = new ArrayList[N+1];
-//		for (int i = 1 ; i <= N; i++) {
-//			graph[i] = new ArrayList<>();
-//		}
-		graph = new int[N+1][N+1];
-//		for (int i = 1 ; i<= N; i++) {
-//			Arrays.fill(graph[i], INF); //(x) 어짜피 이문제에선 가중치0인거 없음 굳이 INF로 구분할필요없이 0으로둬도됨
-//			graph[i][i] = 0; //자기자신
-//		}
+		// 그래프 초기화
+		ArrayList<Node>[] graph = new ArrayList[N+1];
+		for (int i = 1 ; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		// 역방향 계산에 사용될 그래프
+		ArrayList<Node>[] graph2 = new ArrayList[N+1];
+		for (int i = 1 ; i <= N; i++) {
+			graph2[i] = new ArrayList<>();
+		}
+
 		
 		// 입력
 		for (int i = 0 ; i < M ; i++) {
@@ -50,17 +51,19 @@ public class Main {
 			int end = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
 			
-			graph[start][end] = weight;
+			graph[start].add(new Node(end, weight));
+			graph2[end].add(new Node(start, weight)); //역방향용
 		}
 		
-		
+
 		dist1 = new int[N+1]; //전부다 ~ 도착지dest (역방향으로 생각) 도착지dest ~ 전부다
 		dist2 = new int[N+1]; //출발지dest에서 전부다
 		Arrays.fill(dist1, INF);
 		Arrays.fill(dist2, INF);
 		
-		dijkstra(destIdx, dist1, false); //그래프 역방향으로 보기
-		dijkstra(destIdx, dist2, true); //그래프 정방향으로 보기
+		dijkstra(destIdx, dist1, graph2); //그래프 역방향으로 보기
+		dijkstra(destIdx, dist2, graph); //그래프 정방향으로 보기
+		
 		
 		int maxDist = 0;
 		for (int i = 1 ; i <= N ; i++	) {
@@ -71,7 +74,7 @@ public class Main {
 		
 	}
 	
-	static void dijkstra(int start, int[] dist, boolean flag) {
+	static void dijkstra(int start, int[] dist, ArrayList<Node>[] graph) {
 		PriorityQueue<Node> queue = new PriorityQueue<>();
 		
 		dist[start] = 0;
@@ -83,30 +86,17 @@ public class Main {
 			
 			if (dist[cur.idx] < cur.w) continue; // 이미 방문완료한 노드 무시 
 			
-			if (++cnt == N) break; // 종료조건 : 모든 노드 방문함
+			if (++cnt == N) break; // 종료조건(시간단축) : 모든 노드 방문함
 			
-			for (int nextIdx = 1 ; nextIdx <= N; nextIdx++) { //이웃노드 탐색
-				
-				// 자기자신 or 이웃이 아니면 continue
-//				if ( cur.idx == nextIdx ) continue;
-//				if (flag) if (graph[cur.idx][nextIdx] == INF) continue;
-//				else if (graph[nextIdx][cur.idx] == INF) continue;
+			for (Node next : graph[cur.idx]) { //이웃 노드 탐색
+				int nextIdx = next.idx;
+				int stopWeight = next.w; //경유지cur ~ nextIdx 사이 가중치값
 				
 				// 경유지(cur) 거쳐가는 경우 dist배열 업데이트 판단
-				if (flag) {
-					if (graph[cur.idx][nextIdx] == 0) continue;
-					if (dist[nextIdx] > cur.w + graph[cur.idx][nextIdx]) {
-						dist[nextIdx] =  cur.w + graph[cur.idx][nextIdx];
-						queue.offer(new Node(nextIdx, dist[nextIdx])) ; 
-					}
-				} else {
-					if (graph[nextIdx][cur.idx] == 0) continue;
-					if (dist[nextIdx] > cur.w + graph[nextIdx][cur.idx]) {
-						dist[nextIdx] =  cur.w + graph[nextIdx][cur.idx];
-						queue.offer(new Node(nextIdx, dist[nextIdx])) ; 
-					}
+				if (dist[nextIdx] > cur.w + stopWeight) {
+					dist[nextIdx] = cur.w + stopWeight;
+					queue.offer(new Node(nextIdx, dist[nextIdx])); 
 				}
-				
 			}
 			
 		}
