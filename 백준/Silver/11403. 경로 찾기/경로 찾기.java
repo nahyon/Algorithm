e449e28d53d881cf7602feae41e0434ed642db7f
nahyon <62339370+nahyon@users.noreply.github.com>
2025-01-15 18:50:47 +0900

    [Silver I] Title: 경로 찾기, Time: 112 ms, Memory: 13648 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -22,8 +22,7 @@ public class Main {
 		for (int k = 0 ; k < N; k++) {
 			for (int i = 0 ; i < N ; i++) {
 				for (int j = 0 ; j < N; j++) {
-					if (!matrix[i][j] && 
-							(matrix[i][k] && matrix[k][j])) matrix[i][j] = true;
+					if (matrix[i][k] && matrix[k][j]) matrix[i][j] = true;
 				}
 			}
 		}
