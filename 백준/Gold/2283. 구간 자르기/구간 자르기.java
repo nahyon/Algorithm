67e129bca09452c24f793247dec9f7c810aedb65
nahyon <62339370+nahyon@users.noreply.github.com>
2025-02-17 00:00:18 +0900

    [Gold II] Title: 구간 자르기, Time: 108 ms, Memory: 16504 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.java
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.java
@@ -13,51 +13,49 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken()); // N개의 선분 (1이상 1000이하)
 		int K = Integer.parseInt(st.nextToken()); // 길이의 총합 K (1이상 1억이하) 
 	
-		int[][] lineList = new int[N][2];
-		int start=0, end=0;
+		int start=Integer.MAX_VALUE, end=0;
+		int[] cnt = new int[1000002];// 길이 범위 : 0이상 100만이하
 		for (int i = 0 ; i < N ; i++) {
 			st = new StringTokenizer(br.readLine());
 			// 0 이상 1,000,000 이하의 정수
-			lineList[i][0] = Integer.parseInt(st.nextToken()); // 시작
-			lineList[i][1] = Integer.parseInt(st.nextToken()); // 끝
-			start = Math.min(start,  lineList[i][0]);
-			end = Math.	max(end,  lineList[i][1]);
+			int a = Integer.parseInt(st.nextToken()); // 시작
+			int b = Integer.parseInt(st.nextToken()); // 끝
+
+			start = Math.min(start,  a);
+			end = Math.	max(end,  b);
+			
+			///
+	        cnt[a]++;  // 시작점에서 +1
+            cnt[b]--;  // 끝점에서 -1
 		}
-		Arrays.sort(lineList, (o1, o2) -> {return o1[0] - o2[0]; }); //시작점 기준 오름차순 정렬
 		
-		int left,right;
-		left = right = start;
+        for (int i = start+1; i <= end; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        
+		
+		int left = 0; 
+		int right = left+1;
 		
 		boolean flag = false;
-		while (left<=right && right<=end) {
-			// left이상 right이하 사이의 합 구하기
-			
-			int sum = 0;
-			// 완탐으로 합 구하기
-			for (int i = 0 ; i < N ; i++) {
-				// 현재 line : (s, e) 
-				// <-가 left~right사이 얼마나 들어오나
-				int s = lineList[i][0];
-				int e = lineList[i][1];
-				
-				if (right<=s || e<=left) continue; // 구간 밖
-				
-				sum += (Math.min(right, e) - Math.max(left,  s));
-			}
-			
+		
+		int sum = cnt[left]; // 시작 초기화
+		while (left <= right && right<=end+1) {
 			if (sum == K) {
 				flag = true;
 				break;
 			}
 			
-			if (sum > K) left++; // 줄이기
-			else right++; //늘리기
-			
+			if (sum > K) { // 목표K보다 크면 left++
+				sum -= cnt[left];
+				left++;
+			} else { // 목표K보다 작으면 right++
+				sum += cnt[right];
+				right++;
+			}
 		}
 		if (flag) System.out.println(left + " " + right);
 		else System.out.println("0 0");
-		
-		
-		
 	}
 }
