4f3e93990c9ef26a8fc2464cbce85a36d355c9e3
nahyon <62339370+nahyon@users.noreply.github.com>
2025-03-01 17:19:30 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,2 +1,3 @@
 -- 코드를 입력하세요
-SELECT datetime from animal_ins order by datetime desc LIMIT 1;
+# SELECT datetime from animal_ins order by datetime desc LIMIT 1;
+select max(datetime) as "시간" from animal_ins;
