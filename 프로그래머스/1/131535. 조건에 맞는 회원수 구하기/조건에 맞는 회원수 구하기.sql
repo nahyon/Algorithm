af07c02486909dd9ca9dd8794ca6c7408014fb86
nahyon <62339370+nahyon@users.noreply.github.com>
2025-03-01 17:10:30 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,2 +1,2 @@
 -- 코드를 입력하세요
-SELECT count(*) FROM user_info WHERE joined LIKE '2021%' AND age between 20 and 29;
+SELECT count(*) FROM user_info WHERE year(joined) = 2021 AND age between 20 and 29;
