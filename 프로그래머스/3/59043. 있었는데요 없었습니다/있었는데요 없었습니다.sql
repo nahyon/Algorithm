085a49f1bf60d02641f423a7b10f324364828add
nahyon <62339370+nahyon@users.noreply.github.com>
2025-03-09 18:24:45 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT ins.animal_id, ins.name  -- , ins.datetime as 보호시작일, outs.datetime as 입양일
+SELECT ins.animal_id, ins.name  -- , ins.datetime as 보호시작일, outs.datetime as 입양일, timediff(ins.datetime, outs.datetime)
 from animal_ins ins join animal_outs outs
 on ins.animal_id = outs.animal_id
-where timediff(ins.datetime, outs.datetime) > 0
+where ins.datetime > outs.datetime
 order by ins.datetime;
