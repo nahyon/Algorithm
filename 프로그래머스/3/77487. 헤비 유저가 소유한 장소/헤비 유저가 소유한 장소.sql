348745a3c430b96b91c9c5a900a5ccea2d816032
nahyon <62339370+nahyon@users.noreply.github.com>
2025-03-10 08:43:51 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -3,3 +3,13 @@ SELECT id, name, host_id
 FROM places
 where host_id in (select host_id from places group by host_id having count(*) >= 2)
 order by id;
+
+SELECT p.id, p.name, p.host_id
+FROM places p
+JOIN (
+    SELECT host_id
+    FROM places
+    GROUP BY host_id
+    HAVING COUNT(*) >= 2
+) heavy_users ON p.host_id = heavy_users.host_id
+ORDER BY p.id;
