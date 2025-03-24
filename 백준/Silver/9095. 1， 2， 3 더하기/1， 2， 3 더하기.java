f6027ef11bcad4b6143d029bc2aebd8ea3f3494a
nahyon <62339370+nahyon@users.noreply.github.com>
2025-03-24 19:15:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,39 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 백준 9095 _ 1, 2, 3 더하기
+//실버3 _ 1, 2, 3 더하기 
+//DP 
+//코드트리 _ Trail3. Lesson7. 아이템을 적절히 고르는 문제 
 public class Main {
-	static int[] facDP = new int[11] ; //n<11
-	static int a, b, c; //1개수, 2개수, 3개수
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+		
 		StringBuilder sb = new StringBuilder();
+		int[] coins = {1, 2, 3};
 		
-		// 팩토리얼 DP배열 만들기
-		facDP[0] = 1; facDP[1] = 1;
-		for (int i = 2; i < 11; i++ ) {
-			makeDP(i);
-		}
-		
-		for (int i = 0; i < T ; i++) { 
-			int num = Integer.parseInt(br.readLine());
+		for (int t = 0 ; t < T ; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] dp = new int[N+1];
+			dp[0] = 1;
 			
-			int ans = 0 ; 
-			for (c = num/3 ; c >= 0 ; c--) {
-				for (b = (num-3*c)/2 ; b >= 0 ; b--) {
-					a = num - 3*c - 2*b;
-					// (a+b+c)! / (a! * b! * c!)
-					ans += (facDP[a+b+c] / (facDP[a]*facDP[b]*facDP[c]) );
+			for (int i = 1 ; i <= N ; i++) {
+				for (int coin : coins) {
+					if (coin > i) continue;
+					dp[i] += dp[i-coin];
 				}
 			}
-			sb.append(ans).append("\n");
+			sb.append(dp[N]).append("\n");
+			
 		}
 		System.out.println(sb);
-	}
+		
 	
-	static void makeDP(int n) {
-		facDP[n] = n * facDP[n-1];
+
 	}
+
 }
