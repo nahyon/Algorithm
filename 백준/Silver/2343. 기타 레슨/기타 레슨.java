380a7a128eee30edbca04b005a4b67a275435af8
nahyon <62339370+nahyon@users.noreply.github.com>
2025-04-02 22:41:33 +0900

    [Silver I] Title: 기타 레슨, Time: 244 ms, Memory: 22576 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,76 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 백준 2343_ 기타레슨
-// 파라메트릭 탐색
+// 백준 2344 _ 기타레슨 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken()); //강의 수
-		int M = Integer.parseInt(st.nextToken()); // M개
-		
-		// M개안에 최소 length로 강의N개 다 넣기
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
 		int[] videos = new int[N];
-		int total = 0;
+		int left = 0; // 비디오 가장 최소ㄴㄴ 최대  길이 저장
+        int right = 0;
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0 ; i < N ;i++) {
+		for (int i = 0 ; i < N ; i++) {
 			videos[i] = Integer.parseInt(st.nextToken());
-			total += videos[i];
-		}
-		int lo = 0; int hi = total; //0, N-1아니고 최솟값 1이랑 total임 헷갈리지말기
-		int ans = total; //최소를 구해야하니까 최대값으로 설정
-		while (lo <= hi) {
-			int len = (lo+hi)/2; // mid값을 len으로 설정하고
+			left = Math.max(left, videos[i]); // 보다 작으면 잘리는 비디오있으니까 
+		    right += videos[i];
+        }
+		
+		int minLength = Integer.MAX_VALUE; // 블루레이 길이(정답)
+
+		// int right = 1000000000 ;// 최대 10만*1만 = 10억
+		while(left<=right) {
+			int targetLength = (left+right)/2; // 이번에 설정하는 길이
+			
 			
 			int cnt = 0; 
-			int sum = 0;
-			int rest = total;
-			boolean flag = false;
-			for(int i = 0 ; i < N ; i++) { // N개의 비디오에 대해 계산
-				if (videos[i] > len) { //아에 못 담는 경우가 있으면 안됨 -> len 크기 키우기
-					flag = true; 
-					break;
-				}
-				
-				sum += videos[i];
-				if (sum > len) {
-					rest -= (sum-videos[i]);
-					cnt++;
-					
-					sum = videos[i]; 
-				} else if (sum == len) {
-					rest -= sum;
-					cnt++;
-					
-					sum = 0;
-				}
-				
-				
-				if (rest <= len) { // 남은 것들의 총합이 블루레이1개에 들어간다면 cnt+1하고 break
-					cnt++;
-					break;
-				}
-//				if (cnt > M) break;
-			}
-		
-			
-			if ((cnt > M) || flag) { //len크기를 키워야함 -> lo를 뒤로
-				lo = len + 1;
-			} else { // 조건만족. but 이 중에 가장 최솟값을 구해야함(len최대한 최소로)
-				ans = Math.min(ans, len);
-				hi = len-1;
-			}
-		}
-		System.out.println(ans);
-		
-		
-	}
+			int temp = 0;
+			for (int len : videos) { // 비디오 길이 하나씩 보기
 
+				if (temp + len > targetLength ) {
+					cnt++; temp = len;
+				} else if (temp + len == targetLength) {
+					cnt++; temp = 0;
+				} else temp += len;
+				if (cnt > M) break; // 설정길이(targetLength)늘려야함
+			}
+			//** for문 끝까지 다 돌고도 temp가 남아있다면
+			if (cnt <= M  && temp > 0) cnt++; 
+						
+			if (cnt > M) { // targetLength 늘려야함 -> left 오른쪽으로 
+				left = targetLength + 1;
+				// continue; // while문 다시 돌러 
+			} else { // targetLength 줄여야함 -> right 왼쪽으로 
+				minLength = Math.min(minLength, targetLength);
+				right = targetLength - 1;
+			}
+		}	
+
+		System.out.println(minLength);
+
+	}
 }
