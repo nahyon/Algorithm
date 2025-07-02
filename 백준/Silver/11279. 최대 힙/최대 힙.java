3d4f81f7c96fa84dcaa44ff4ce639b41e912754b
nahyon <62339370+nahyon@users.noreply.github.com>
2025-07-02 18:36:25 +0900

    [Silver II] Title: 최대 힙, Time: 252 ms, Memory: 25480 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,6 +1,11 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+// PQ - 내림차순 정렬 
+// 백준 11279 _ 최대힙
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -8,13 +13,13 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 
 		StringBuilder sb = new StringBuilder();
-		Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		Queue<Integer> pq = new PriorityQueue<>();
 		for (int i = 0 ; i < N ; i++) {
 			int x = Integer.parseInt(br.readLine());
 			if (x == 0) {
 				if (pq.isEmpty()) sb.append(0).append("\n");
-				else sb.append(pq.poll()).append("\n");
-			} else pq.offer(x);
+				else sb.append(-pq.poll()).append("\n");
+			} else pq.offer(-x);
 		}
 		System.out.println(sb);
 	}
