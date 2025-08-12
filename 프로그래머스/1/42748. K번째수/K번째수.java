ca85b801fb7d990255340562e3579323d40ec147
nahyon <62339370+nahyon@users.noreply.github.com>
2025-08-12 16:03:16 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0 ; i < commands.length; i++) {
+            int[] command = commands[i];
+            int[] arr = Arrays.copyOfRange(array, command[0]-1, command[1] );
+            Arrays.sort(arr); // 정렬 
+            answer[i] = arr[command[2]-1];
+
+        }
+        return answer;
+    }
+}
