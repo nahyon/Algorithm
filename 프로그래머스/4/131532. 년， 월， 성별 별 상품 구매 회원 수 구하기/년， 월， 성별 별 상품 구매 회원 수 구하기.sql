53d9001453744d44e5ad076e0805d99c8f30804a
nahyon <62339370+nahyon@users.noreply.github.com>
2025-09-10 20:23:26 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,10 +1,16 @@
--- 코드를 입력하세요
-SELECT year(b.sales_date) as year,  month(b.sales_date) as month, 
-a.gender, count(distinct(a.user_id)) as users
-FROM user_info a
-# FROM (select * from user_info where gender is not null) a
-join online_sale b
-on a.user_id = b.user_id
-where a.gender is not null
-group by year(b.sales_date), month(b.sales_date), a.gender
-order by year(b.sales_date), month(b.sales_date), a.gender;
+# -- 코드를 입력하세요
+# SELECT year(b.sales_date) as year,  month(b.sales_date) as month, 
+# a.gender, count(distinct(a.user_id)) as users
+# FROM user_info a
+# # FROM (select * from user_info where gender is not null) a
+# join online_sale b
+# on a.user_id = b.user_id
+# where a.gender is not null
+# group by year(b.sales_date), month(b.sales_date), a.gender
+# order by year(b.sales_date), month(b.sales_date), a.gender;
+
+select year(sale.sales_date) as YEAR, month(sale.sales_date) as MONTH, info.gender as GENDER, count(distinct info.user_id) as users
+from user_info info join online_sale sale on sale.user_id = info.user_id
+where info.gender is not null
+group by year(sale.sales_date) , month(sale.sales_date), info.gender
+order by year(sale.sales_date) , month(sale.sales_date), info.gender;
